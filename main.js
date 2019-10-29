f0375c3f5ee1e8dc0de4b25c7e08dde3d72e5c7b
Willemijn <willemijn@bresjer.nl>
2019-10-29 10:42:31 +0100

    Add my solution

--- main.js
+++ main.js
@@ -9,3 +9,23 @@ Once we have converted text to the whale language, the result is sung slowly, as
 
 To accomplish this translation, we can use our knowledge of loops. Let’s get started! */
 
+let input = "turpentine and turtles";
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for ( let i = 0 ; i < input.length; i++ ) {
+  for ( let j = 0; j < vowels.length; j++ ) {
+    // console.log(`j is ${j}`);
+    if ( input[i] === vowels[j] ) {
+        resultArray.push(vowels[j]);
+        };
+  }
+     if ( input[i] === 'e' ) {
+        resultArray.push(input[i]);
+        }
+     if ( input[i] === 'u' ) {
+        resultArray.push(input[i]);
+        }
+  // console.log(`i is ${i}`);
+}
+console.log(resultArray.join('').toUpperCase());
